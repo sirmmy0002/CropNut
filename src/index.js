f41e564be8d67ada28e9b6f5e62d8e84fa72ba35
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-25 07:50:53 +0300

    project

--- src/index.js
+++ src/index.js
@@ -5,3 +5,21 @@ let greet=document.querySelector("#greeting")
 function greetings(){
   greet.innerHTML="Hello "+ input.value+",welcome to CropNut we are happy to host you!"
 }
+ 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '05ba7b9cabmsh705315888b0430ep16a29cjsn68fbf166675f',
+		'X-RapidAPI-Host': 'world-ecommerce.p.rapidapi.com'
+	}
+};
+
+fetch('https://world-ecommerce.p.rapidapi.com/?url=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB08L5RJ392', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+  function display(product)
+  {
+    const shopContent = document.getElementById
+  }
