45861e364f77408b079db03d1065f767c2f4fbbf
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-25 08:34:58 +0300

    project

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ As a user of the application,you will be able to :
  * Access to internet
  ### Installation Process
  ****
-* Clone to the repo : git clone 
+* Clone to the repo : git clone https://github.com/sirmmy0002/CropNut/tree/working
 * Unzip the downloaded files in a folder of choice.
 * Open the index file from the zipped file with any browser.
  ****
--- src/index.js
+++ src/index.js
@@ -6,20 +6,16 @@ function greetings(){
   greet.innerHTML="Hello "+ input.value+",welcome to CropNut we are happy to host you!"
 }
  
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '05ba7b9cabmsh705315888b0430ep16a29cjsn68fbf166675f',
-		'X-RapidAPI-Host': 'world-ecommerce.p.rapidapi.com'
-	}
-};
 
-fetch('https://world-ecommerce.p.rapidapi.com/?url=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB08L5RJ392', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-  function display(product)
-  {
-    const shopContent = document.getElementById
-  }
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '05ba7b9cabmsh705315888b0430ep16a29cjsn68fbf166675f',
+      'X-RapidAPI-Host': 'malamal-data-for-ecommerce.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://malamal-data-for-ecommerce.p.rapidapi.com/product/B01MXL4ANH/offers?api_key=bd2a7b5398c95efa6b82ceb23cf7416e', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
